96da6508d1188e7970db15b62aba1f8e786bd0f2
Roman Dutchak <rd@newagesol.com>
2018-09-04 12:09:32 +0300

    Change hash

--- clif.rb
+++ clif.rb
@@ -2,7 +2,7 @@ class Clif < Formula
     desc "Bookmarks for cli commands"
     homepage "https://github.com/dutchakdev/clif"
     url "https://github.com/dutchakdev/clif/releases/download/v0.0.1/clif_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 "78599eb003d812784dfe20230c9e6b87deb2d028d06dcafabdc50cfcd82a142e"
+    sha256 "3ee23d44ccf4a3e020d4bccaa4268f8efc9e08f8dadca2a90a3a7db36137d35e"
   
     def install
         bin.install "clif"
